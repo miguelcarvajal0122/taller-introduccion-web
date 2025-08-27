e77be5c4b168bdbd99a725ed64eb2cf50fc3c1df
Miguelcarvajal0122 <cardenasmiguel011022@gmai.com>
2025-08-27 15:14:50 -0500

    Parte práctica: botón y mensaje en consola con JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+function saludar() {
+console.log("Hola Mundo desde JavaScript");
+alert("Hola Mundo desde JavaScript 👋");
+}
+
+
+document.getElementById("btnSaludo").addEventListener("click", saludar);
